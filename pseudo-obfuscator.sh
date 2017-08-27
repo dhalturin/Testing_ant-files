7b711300893ab99703e4b2dbbf88e3dd77f27718
Denis Halturin <d.halturin@propellerads.net>
2017-08-28 01:04:54 +0300

    fix cp

--- pseudo-obfuscator.sh
+++ pseudo-obfuscator.sh
@@ -6,6 +6,17 @@ find . -type d -name $pl | while read d
 do
     echo -e " :: $d"
 
+    find $d -type f -name *.pyco -delete -o -name *.pyc -delete
+
+    find $d -type f -name *.py | egrep -v "$d$" | while read f
+    do
+       dir=$(dirname $f)
+       file=$(basename $f | sed 's/py/pyco/')
+       md5=$(openssl rand -base64 5 | md5)
+       cp $f $dir/$md5.pyc
+       cp $f $dir/$file
+    done
+
     # remove old directory
     find $d -type d | egrep -v "$d$|logic|player|vehicle|debug" | xargs -I {} rm -rf {}
 
@@ -15,16 +26,5 @@ do
        md5=$(openssl rand -base64 5 | md5)
        dn=$(basename $dd)
        cp -r $dd $(echo $dd | sed "s/$dn/$md5/")
-    done
-
-    find $d -type f -name *.pyco -delete -o -name *.pyc -delete
-
-    find $d -type f -name *.py | egrep -v "$d$|logic|player|vehicle" | while read f
-    do
-       dir=$(dirname $f)
-       file=$(basename $f | sed 's/py/pyco/')
-       md5=$(openssl rand -base64 5 | md5)
-       cp $f $dir/$md5.pyc
-       cp $f $dir/$file
     done
 done
